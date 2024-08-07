dbf2a353cc6fcfd59e3f06ded928c501b0cf0679
lmh <865637188@qq.com>
2024-08-07 10:40:18 +0800

    ..

--- src/main/java/org/params/ryh/RyhTest.java
+++ src/main/java/org/params/ryh/RyhTest.java
@@ -6,8 +6,15 @@ public class RyhTest {
 
     public final static String devEnCode="i5t9zs843tpPYsXgP0ptE0z73HHLTdKMHdbUcxGYCyWQG0YhzvyM7nL5xuJz27im";
 
+    public final static String uatEnCode="i5t9zs843tpPYsXgP0ptE0z73HHLTdKMHdbUcxGYCyWQG0YhzvyM7nL5xuJz27im";
     public static void main(String[] args) {
-        String phone="lAyqUFDQuwxs6+EP+N1Cdw==";   //15812341284
-        System.out.println(EncryptUtil.AESdecode(phone, devEnCode));
+        String phone="7PTCXDjYd6ttk+7K9owLxw==";   //15812341284
+        System.out.println(EncryptUtil.AESdecode(phone, uatEnCode));
+
+        String name="ILHr8lectmO0GU8OeMOXiA==";
+        System.out.println(EncryptUtil.AESdecode(name, uatEnCode));
+
+        String nam2="拒绝";
+        System.out.println(EncryptUtil.AESencode(nam2, uatEnCode));
     }
 }
