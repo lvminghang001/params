227e2204d5ff27afef02cd755ffa90aeffcb27bc
lmh <865637188@qq.com>
2026-01-14 09:51:33 +0800

    ..

--- src/main/java/org/params/bjxd/BjxdController.java
+++ src/main/java/org/params/bjxd/BjxdController.java
@@ -1,5 +1,6 @@
 package org.params.bjxd;
 
+import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.http.HttpRequest;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,8 @@ public class BjxdController {
 
     // 请求参数
     // 192.168.100.218:10086/bjxd/callBack?prod=jxhln
+    public String aesKey = "ByqenvI0IVAKluGl";
+
     public static String reqParam = """
             {
                 "data": {
@@ -29,14 +32,17 @@ public class BjxdController {
     @PostMapping(value = "/callBack")
     public String callBack(@RequestBody  String json,String prod) throws IOException{
         String timestamp = String.valueOf(System.currentTimeMillis());
-        String body = BaJieXiaoDaiUtil.encrypt(json,"ByqenvI0IVAKluGl");
+        String body = BaJieXiaoDaiUtil.encrypt(json,aesKey);
         String nonce = "wwb";
         String sha1 = BaJieXiaoDaiUtil.getSHA1(BaJieXiaoDaiConstants.TOKEN_TEST, timestamp, nonce, body);
         StringBuilder builder = new StringBuilder(String.format("https://%s.hzbxhd.com/prod-api/market/callback/bjxd/feishuCallBack?timestamp=%s&nonce=%s&msg_signature=%s"
         ,prod,timestamp,nonce,sha1));
         String result = HttpRequest.post(builder.toString()).body(body).header("Content-Type", "application/json").timeout(10000).execute().body();
-        String enData =  BaJieXiaoDaiUtil.decrypt(result,"ByqenvI0IVAKluGl");
-        return enData;
+        if(ObjectUtil.isNotEmpty(result) && result.contains("令牌")){
+            System.out.println("鉴权失败，应该是未开放白名单");
+            return "鉴权失败，应该是未开放白名单";
+        }
+        return BaJieXiaoDaiUtil.decrypt(result,aesKey);
     }
 
 }
